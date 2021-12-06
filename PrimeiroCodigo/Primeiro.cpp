93f75657bfa715dd9996b962573f76bded8a161d
Eduardo <edu3839@hotmail.com>
2021-12-06 10:38:53 -0300

    Estudos

---
+++ PrimeiroCodigo/Primeiro.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "Vou" << "\n";
+	std::cout << "Aprender" << "\n";
+	std::cout << "Programar" << "\n";
+	std::cout << "Na linguagem" << "\n";
+	std::cout << "C++" << "\n";
+
+	system("color 38");
+	system("pause");
+	return 0;
+
+}
+	//Eduardo Andarde - 02/12/2021
+	/*A finalidade deste programa simples, é para atender a demanda de um novo curso
+	que eu iniciei, para dominar, ou pelo menos avançar em programação no geral.
+	Tambem deixo registrado que utilizei as teclas atalhos para gerar estes comentarios.*/
